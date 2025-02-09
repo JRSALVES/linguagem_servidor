d9c44eff2ae3fc3a5ea2474363a1e4ef82c07bcf
JRSALVES <jeruso.nascimento@outlook.com>
2025-02-09 16:10:20 -0300

    Criado as pastas por assunto.

---
+++ program/funcoes/minhaFuncao.php
@@ -0,0 +1,10 @@
+<?php
+//Declarando a funcao e povoando-a
+function dizer_ola_mundo(){
+    echo "Olá mundo!<br>";
+}
+
+// Chamadas da função espera a saida "Olá mundo! 2x.
+dizer_ola_mundo();
+dizer_ola_mundo();
+?>
