7b31966d14c9f3073d58b6186727befdf8dd7965
JRSALVES <jeruso.nascimento@outlook.com>
2025-02-12 13:10:09 -0300

    atualização arquivos de texto

--- program/condicoes/cod12Formulario.php
+++ program/condicoes/cod12Formulario.php
@@ -6,16 +6,16 @@ require 'inicio.php';
 
 <?php 
 if(isset($_POST['nome_usuario'])){
-    echo '<h2>Olá, ' . $_POST['nome_usuario']. '</h2>';
+    echo '<h2>Olá, ' . htmlspecialchars($_POST['nome_usuario']) . '</h2>';
 }
 ?>
 
-<form>
+<form method="POST">
     <label for="campo_de_nome">Digite o seu nome:</label>
-    <input type= "text"name="nome_usuario" id="campo_de_nome" />
+    <input type="text" name="nome_usuario" id="campo_de_nome" />
     <button type="submit">Enviar</button>
 </form>
 
 <?php 
-require 'fim'
+require 'fim.php';
 ?>
--- program/variaveis/index.php
+++ program/variaveis/index.php
@@ -1,11 +1,7 @@
 <?php
-require 'inicio.php';
+
 $idade = 2020 - 1985;
 $nome = 'Jeruso';
 ?>
-
 <h1>Nome: <?php echo $nome . ' ' . $idade; ?> anos de idade</h1>
 
-<?php
-    require 'fim.php';
-?>
